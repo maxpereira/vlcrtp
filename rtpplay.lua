958bc0f51aa3b2e5dc56f8a96cca2407000c2625
Max Pereira <max@pereira.at>
2020-07-30 13:54:03 -0700

    Initial commit

---
+++ rtpplay.lua
@@ -0,0 +1,44 @@
+-- RTP Play Extension for VLC
+-- Author: Max Pereira
+-- Github: github.com/maxpereira
+
+function descriptor()
+    return { title="RTP Play" }
+end
+
+function activate()
+	vlc.msg.dbg("[RTP Play Extension] Starting")
+end
+
+function deactivate()
+	vlc.msg.dbg("[RTP Play Extension] Stopping")
+end
+
+function main()
+	rtpPLAY = NewRTPplay()
+	rtpPLAY.populateChannels()
+end
+
+function NewRTPplay()
+	local populateChannels = function ()
+		vlc.sd.add_item( {path = "https://streaming-live.rtp.pt/liveradio/antena180a/playlist.m3u8?DVR",
+											title = "RTP ANTENA 1",
+											arturl = "https://empathogen.net/a1.png"
+									})
+									
+		vlc.sd.add_item( {path = "https://streaming-live.rtp.pt/liveradio/antena280a/playlist.m3u8?DVR",
+											title = "RTP ANTENA 2",
+											arturl = "https://empathogen.net/a2.png"
+									})
+
+		vlc.sd.add_item( {path = "https://streaming-live.rtp.pt/liveradio/antena380a/playlist.m3u8?DVR",
+											title = "RTP ANTENA 3",
+											arturl = "https://empathogen.net/a3.png"
+									})										
+	end
+
+	return {
+		populateChannels = populateChannels
+	}
+end
+
